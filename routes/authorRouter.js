f6364dd9bfb6f79a0c3940a3496dbdefed8cb595
sahmedjaffer <s.ahmedjaffer@gmail.com>
2025-05-24 14:55:03 +0300

    update controllers and views with validation

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const authorController = require('../controllers/authorController.js')
-
+const Author = require('../models/Author.js')
 
 router.get('/', authorController.listAllAuthors);
 router.get('/:id', authorController.listAuthorById);
 router.put('/:id',authorController.updateAuthor);
 router.delete('/:id', authorController.deleteAuthor);
+
+router.get('/:id/edit', async (req, res) => {
+  const authors = await Author.findById(req.params.id).populate('works');
+  res.render('../views/authors/editAuthor.ejs', ({authors}))
+})
+
+
 
 module.exports = router;
 
