a3640086044aa874b1f3fe7c8f9df735bce65267
Sayed Ahmed Jaafar <s.ahmedjaffer@gmail.com>
2025-05-27 10:00:12 +0000

    update in book page and put some enhansments

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -78,36 +78,44 @@ const createNewBook = async (req, res) => {
 };
 
 
-const listAllBooks = async (req, res) => {
-    try {
+// const listAllBooks = async (req, res) => {
+//     try {
           
-      const findAllBooks = await Book.find().populate('author');
+//       const findAllBooks = await Book.find().populate('author');
       
-      if(!findAllBooks){
+//       if(!findAllBooks){
       
-        res.render('books/bookNotFound');
+//         res.render('books/bookNotFound');
       
-      }else{
+//       }else{
     
-        let userRole = null;
+//         let userRole = null;
     
-        if (req.session.user && req.session.user._id) {
+//         if (req.session.user && req.session.user._id) {
     
-          userRole = await User.findById(req.session.user._id);
+//           userRole = await User.findById(req.session.user._id);
     
-        }
+//         }
+//         const author = findAllBooks.find(book => book.author && book.author.name);
+        
+//         console.log(chalk.blue('Author name: ', author.name));
 
-        res.render('books/allBooks', { findAllBooks, userRole });  
     
-      }
-    }
+
+//         console.log();
+//        // if(!author){
+
+//         res.render('books/allBooks', { findAllBooks, userRole });  
     
-    catch (error) {
+//       }
+//     }
     
-      console.error(`${chalk.red('Error occurred in listing all book ', error.message)}`)
+//     catch (error) {
     
-    } 
-  }
+//       console.error(`${chalk.red('Error occurred in listing all book ', error.message)}`)
+    
+//     } 
+//   }
 
 
 
@@ -193,12 +201,92 @@ const newBookPage = async (req, res) => {
   }
 }
 
-module.exports = {
-    listAllBooks,
+
+
+
+const searchBooks = async (req, res) => {
+    try {
+            const filter = req.query.all || 'all';
+      const findByTitle = req.query.title || '';
+      const findByCategory = req.query.category || '';
+      const findByPublisher = req.query.publisher || '';
+      const findByIsbn = req.query.isbn || '';
+      const findByAuthor = req.query.author || ''; 
+              let userRole = null;
+    
+        if (req.session.user && req.session.user._id) {
+    
+          userRole = await User.findById(req.session.user._id);
+        }
+
+      const showAllBooks = await Book.find({}).populate('author');
+      res.render('books/allBooks', { showAllBooks,userRole });
+
+
+
+
+
+
+      // Check if user is authenticated
+
+            // Check if user has a valid role
+
+
+        
+            // Check if user has a valid search term
+           
+            if (!filter || filter === 'all') {
+              // If filter is 'all', return all books
+              const showAllBooks = await Book.find({}).populate('author');
+              return res.render('books/allBooks', { showAllBooks,userRole });
+            }
+            if (filter === 'author' && findByAuthor) {
+              // If filter is 'author', return books by author
+              const showAllBooks = await Book.find({ author: findByAuthor }).populate('author');
+              return res.render('books/allBooks', { showAllBooks,userRole });
+            }
+            if (filter === 'title' && findByTitle) {
+              // If filter is 'title', return books by title
+              const showAllBooks = await Book.find({ title: findByTitle }).populate('author');
+              return res.render('books/allBooks', { showAllBooks,userRole });
+            }
+            if (filter === 'category' && findByCategory) {
+              // If filter is 'category', return books by category
+              const showAllBooks = await Book.find({ category: findByCategory }).populate('author');
+              return res.render('books/allBooks', { showAllBooks,userRole });
+            }
+            if (filter === 'publisher' && findByPublisher) {
+              // If filter is 'publisher', return books by publisher
+              const showAllBooks = await Book.find({ publisher: findByPublisher }).populate('author');
+              return res.render('books/allBooks', { showAllBooks,userRole });
+            }
+            if (filter === 'isbn' && findByIsbn) {
+              // If filter is 'isbn', return books by isbn
+              const showAllBooks = await Book.find({ isbn: findByIsbn }).populate('author');
+              return res.render('books/allBooks', { showAllBooks,userRole });
+            }
+            // If filter is not valid, return an error message
+            if (!['all', 'author', 'title', 'category', 'publisher', 'isbn'].includes(filter)) {
+              return res.render('books/bookNotFound');
+            }
+            // If filter is 'all', return all books
+
+        
+          
+          }catch (error) {
+        console.error(`${chalk.red('Error occurred in searching books ', error.message)}`);
+        return res.status(500).send('Internal Server Error');
+    };     
+}
+
+
+    module.exports = {
+    //listAllBooks,
     listBookById,
     updateBook,
     deleteBook,
     createNewBook,
     editBookPage,
-    newBookPage
+    newBookPage,
+    searchBooks
 }
